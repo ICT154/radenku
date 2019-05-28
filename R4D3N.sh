828a3e4fd3e3da3cea56e818480f455471b66af6
Islamic Cyber Team <radengozal@gmail.com>
2019-05-28 14:22:23 +0700

    Create R4D3N.sh

---
+++ R4D3N.sh
@@ -0,0 +1,38 @@
+clear
+cat den.txt
+echo "===================================" 
+echo "Tools Termux Multi Fungsi" | lolcat
+echo "Author : R4D3N" | lolcat
+echo "Team : Baku Hantam Cyber Team" | lolcat
+echo "Find Me On Fb : Raden Gozal" | lolcat
+echo "==================================="
+echo ""
+echo "-----------------------------  -----------------------------      " | lolcat 
+echo "[1] . Hack FB narget           [16] . Yahoo Cloning Terbaru   "
+echo "-----------------------------  -----------------------------      " | lolcat
+echo "[2] . Hack IG narget           [17] . Profile Guard FB"
+echo "-----------------------------  -----------------------------     " | lolcat
+echo "[3] . Hack GMAIL narget        [18] . Sadap Kamera Jarak Jauh" 
+echo "-----------------------------  -----------------------------     " | lolcat
+echo "[4] . Whatsapp Exploiter       [19] . Sadap Pesan Jarak Jauh"
+echo "-----------------------------  -----------------------------     " | lolcat
+echo "[5] . Top Up Ilegal ML         [20] . IP TRACKER (LACAK ORG)"
+echo "-----------------------------  -----------------------------     " | lolcat
+echo "[7] . Top Up Ilegal Free Fire  [21] . Download Lagu dari YT"
+echo "-----------------------------  -----------------------------      " | lolcat
+echo "[8] . Auto Follow IG           [22] . Terror Email Orang"
+echo "-----------------------------  -----------------------------       " | lolcat
+echo "[9] . Auto Like FB             [23] . Hack Printer"
+echo "-----------------------------  -----------------------------     " | lolcat
+echo "[10] . Bot Komen FB            [24] . Auto Report FB"
+echo "-----------------------------  -----------------------------     " | lolcat
+echo "[11] . Bot IG                  [25] . KK & KTP GENERATOR"
+echo "-----------------------------  -----------------------------     " | lolcat
+echo "[12] . Hack CCTV               [26] . DDOS BY R4D3N"
+echo "-----------------------------  -----------------------------     " | lolcat
+echo "[13] . Hack Pulsa              [27] . DEFACE BY R4D3N"
+echo "-----------------------------  -----------------------------     " | lolcat
+echo "[14] . NEW MBF                 [28] . Bikin SC DEFACE"
+echo "-----------------------------  -----------------------------     " | lolcat
+echo "[15] . SMS GRATIS              [29] . Install J4NC0K tools"
+echo "-----------------------------  -----------------------------     " | lolcat
